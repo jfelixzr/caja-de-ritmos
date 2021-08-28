04e0db86af56e8f75b02e25e266c128e7675d8f6
Jose Felix <86790583+jfelixzr@users.noreply.github.com>
2021-08-28 16:11:31 -0500

    fixing things

--- src/App.jsx
+++ src/App.jsx
@@ -2,80 +2,72 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
 
+const iniState=
+  `
+  estos es un parrafo
+  
+  **esto es un texto con negrita**
+  >blockquote
+  # heading
+  ## heading 2
+  - list a 
+  
+  - list b 
+  
+  - list c 
+  
+  [my website](https://google.com)
+  
+  this is a inline \`<div></div>\`
+  
+  this is a block of code
+  \`\`\`
+  let n1=5;
+  let n2=10;
+  let res=n1+n2;
+   \`\`\`
+   ![me](https://avatars.githubusercontent.com/u/86790583?v=4)
+   `;
 
-const arreglo = 'https://gist.githubusercontent.com/gejocad/f4914ce6eb4412909e6ebf8e61d6729f/raw/9cee3f9e72a911f3e69d58713821d349f343491b/frases.json';
 
-class Frases extends Component {
-  state = {
-    frases: [
-      {
-        frase: "",
-        autor: ""
-      }
-    ],
-    cuenta: 0
+class MarkDown extends React.Component {
+  state={
+    text:iniState
   }
-
-  componentDidMount() {
-      
-    fetch(arreglo).then(res => res.json())
-      .then(res => {
-        this.setState({
-          frases: res.frases
-        }, this.activador);
-    });
+  handleChange=(e)=>{
+    this.setState({
+      text:e.target.value
+    })
   }
-
-  activador = () => {
-    const { frases } = this.state;
-    
-    if(frases.length > 0) {
-      const cuenta = Math.floor(Math.random() * frases.length);
-      this.setState({
-        cuenta
-      })
-    }
-  }
-
-  render() {
-    const { frases, cuenta } = this.state;
-    
-    const frase = frases[cuenta];
-    
-    const tweetURL = `https://twitter.com/intent/tweet?text=${frase.frase} - ${frase.autor}`;
-    
+  render(){
+    const {text}=this.state;
+    const markdown=marked(text, {breaks:true});
     return (
-      <div className="contenedor">
-        <div className="col-5 marco" id="quote-box">
-          {
-            frase && (
-              <div className="mb-4">
-                <h5 id="text" class="text-large">
-                  <i className="fas fa-2"></i>
-                  {frase.frase}
-                </h5>
-                <cite className="d-block text-right" id="author">
-                  - {frase.autor}
-                </cite>
-              </div>
-              )
-            }
-          <div className="d-flex justify-content-between">
-            <a className="btn btn-primary"
-              target="blank" href={tweetURL} id="tweet-quote">
-              <i className="fab fa-twitter"></i> Tweetear
-            </a>
+    <div>
+        <h1 className="text-center m-4">MarkDown</h1>
+        <div className="row">
+          <div className="col-6">
+            <h3>Texto:</h3>
+            <textarea id="editor" className="form-control" value={text} onChange={this.handleChange} />
+          </div>
+          
+          <div className="col-6 " >
+             <h3>Resultado:</h3>
+            <div className="previewer" dangerouslySetInnerHTML={{__html: markdown}} id="preview">
             
-            <button className="btn btn-success" 
-              onClick={this.activador} id="new-quote">
-              <i className="fas fa-random"></i> Siguiente
-            </button>
           </div>
         </div>
-      </div>
+        </div> 
+         </div>
+      
+        
+    
     )
+    
   }
 }
 
 
-export default Frases;
+
+
+
